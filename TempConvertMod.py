8f996df2742ffcc6604acf15f4eed94a39f93fb5
James Hinson <octobersith@outlook.com>
2022-07-13 00:39:53 -0400

    TempConvert Complete

--- TempConvertMod.py
+++ TempConvertMod.py
@@ -3,10 +3,10 @@
 
 #(32°F − 32) × 5/9 = 0°C
 def convertCelsius(temperature):
-    float((temperature - 32) * (5/9))
-    return
+    return float((temperature - 32) * (5/9))
+    
 
 #(32°C × 9/5) + 32 = 89.6°F
 def convertFahrenheit(temperature):
-    float((temperature * (9/5)) + 32)
-    return
+    return float((temperature * (9/5)) + 32)
+    
