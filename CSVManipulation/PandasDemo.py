72ebf20b73dd5240131fb08c78597958ecb116a2
James Hinson <octobersith@outlook.com>
2022-07-17 03:11:18 -0400

    Pandas update

--- CSVManipulation/PandasDemo.py
+++ CSVManipulation/PandasDemo.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
+#Pandas DataFrame
 df = pd.read_csv('CSVManipulation/GaCovidData.csv')
-
 print(df.head())
 print(df.tail())
+date_myBirthday = df[(df['state'] == 'GA')]
+print(len(date_myBirthday))
+
+
